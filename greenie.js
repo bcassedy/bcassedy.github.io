3e50524614c7460860e1940f650404bdc32db734
Brett Cassedy <brett.cassedy@gmail.com>
2014-06-02 23:45:16 -0700

    Initial commit

---
+++ greenie.js
@@ -0,0 +1,35 @@
+(function(root) {
+  var Asteroids = root.Asteroids = (root.Asteroids || {})
+
+  Function.prototype.inherits = function (superClass) {
+    function Surrogate () {};
+    Surrogate.prototype = superClass.prototype;
+    this.prototype = new Surrogate();
+  }
+
+
+  var MovingObject = Asteroids.MovingObject;
+
+  var Greenie = Asteroids.Greenie = function () {
+    MovingObject.apply(this, arguments);
+    this.COLOR = 'green';
+    this.radius = 5;
+    this.fillStyle = 'green';
+  }
+
+  Greenie.inherits(MovingObject);
+
+  Greenie.prototype.randomGreenie = function (dimX, dimY) {
+    var r = 5;
+    return new Greenie(
+      (dimX - 2*r)* Math.random() + r,
+      (dimY - 2*r) * Math.random() + r,
+      0,
+      0,
+      r,
+      'green'
+    );
+  }
+
+
+})(this);
